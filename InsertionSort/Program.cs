489e50479ddad3dfdc37b4a277bd2a549ce321ea
berkant <berkant@kardelenyazilim.com>
2020-06-13 20:11:22 +0300

    Add project files.

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] theIntArray = new int[] { 2, 40, 15, 7, -1, 0, 12 };
+
+            SortUtil<int>.PrintArray(ref theIntArray);
+
+            var sortedInt = SortUtil<int>.InsertionSort(theIntArray);
+
+            SortUtil<int>.PrintArray(ref sortedInt);
+
+            string[] theStringArray = new string[] { "A", "Z", "K", "H", "P", "J" };
+            SortUtil<string>.PrintArray(ref theStringArray);
+            var sortedString = SortUtil<string>.InsertionSort(theStringArray);
+
+            SortUtil<string>.PrintArray(ref sortedString);
+
+
+
+        }
+    }
+}
---
+++ InsertionSort/SortUtil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace InsertionSort
+{
+    public static class SortUtil<T> where T : IComparable
+    {
+        public static void PrintArray(ref T[] array)
+        {
+            Console.WriteLine(nameof(@array) + "-------");
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static T[]  InsertionSort(in T[] A )
+        {           
+            T[] result=new T[A.Length];
+            Array.Copy(A, result, A.Length);
+            for (int j = 1; j < result.Length; j++)
+            {
+                T key = result[j];
+                var i = j - 1;
+                while (i > -1 && result[i].CompareTo(key) > 0)
+                {
+                    result[i + 1] = result[i];
+                    i--;                  
+                }
+                result[i + 1] = key;
+
+            }
+
+            return result;
+
+        }
+    }
+}
